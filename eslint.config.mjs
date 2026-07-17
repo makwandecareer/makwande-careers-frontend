aae3212059865e86e812e372c424241035e9ff90
Makwande Gcora <admin@makwandecareer.co.za>
2026-07-17 12:02:56 +0200

    Stabilize Windows build and lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,11 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 export default defineConfig([
   ...nextVitals,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "warn",
+      "@next/next/no-img-element": "warn",
+    },
+  },
   globalIgnores([".next/**", "node_modules/**"]),
 ]);
